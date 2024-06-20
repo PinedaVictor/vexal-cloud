c3c29423342a4cedf7bf9c939c032103b56c9958
PinedaVictor <pinedavictor095@gmail.com>
2024-06-19 22:29:22 -0700

    Removed unnecessary dependencies, and cleaned up code

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,56 +1,10 @@
 "use client";
-import "firebaseui/dist/firebaseui.css";
-import { useEffect } from "react";
-import { vexalFirebaseApp } from "./firebase";
-import { useRouter } from "next/navigation";
-import { getAuth, GoogleAuthProvider } from "firebase/auth";
-import * as firebaseui from "firebaseui";
 
 export default function Home() {
-  const router = useRouter();
-  const auth = getAuth(vexalFirebaseApp);
-
-  useEffect(() => {
-    const uiConfig = {
-      signInOptions: [
-        GoogleAuthProvider.PROVIDER_ID,
-        // List of OAuth providers supported.
-        // firebase.auth.FacebookAuthProvider.PROVIDER_ID,
-        // firebase.auth.TwitterAuthProvider.PROVIDER_ID,
-        // firebase.auth.GithubAuthProvider.PROVIDER_ID,
-      ],
-    };
-    let ui: firebaseui.auth.AuthUI | null;
-    // Check if an AuthUI instance already exists
-    if (!firebaseui.auth.AuthUI.getInstance()) {
-      ui = new firebaseui.auth.AuthUI(auth);
-    } else {
-      ui = firebaseui.auth.AuthUI.getInstance();
-    }
-
-    if (ui == null) {
-      return;
-    }
-    ui.start("#firebaseui-auth-container", uiConfig);
-    // Handle redirect on FirebaseUI user change event
-    const unregisterAuthObserver = auth.onAuthStateChanged((user) => {
-      if (user) {
-        router.push("/dashboard" || "/"); // Redirect to signInSuccessUrl or fallback to '/'
-      }
-    });
-
-    return () => {
-      unregisterAuthObserver(); // Clean up Firebase auth observer
-      ui!.reset(); // Reset FirebaseUI instance
-    };
-  }, [auth, router]);
   return (
     <body>
       <div className=" bg-slate-100 flex justify-center items-center min-h-screen">
         <div>Hello from Vexal</div>
-        <div>
-          <div id="firebaseui-auth-container" />
-        </div>
       </div>
     </body>
   );
