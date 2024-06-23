4e16d1a5165ca50db6f94dd3015ac7e10731d09a
PinedaVictor <pinedavictor095@gmail.com>
2024-06-23 10:01:49 -0700

    Added function comment to redirectUser server action

--- src/app/api/actions/redirect.ts
+++ src/app/api/actions/redirect.ts
@@ -2,6 +2,12 @@
 import { NextResponse } from "next/server";
 import { redirect } from "next/navigation";
 
+/**
+ *
+ * @param token
+ * @param userId
+ * @returns a redirect to the callback URL along with the token and userId provided
+ */
 export const redirectUser = async (token: string, userId: string) => {
   const callbackUrl = process.env.CALLBACK_URL;
 
